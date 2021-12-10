22c3edd0696656e8b87bf52201f9d279b3ab81f2
Himanshu <codebreaker8055@gmail.com>
2021-12-10 22:02:41 +0330

    C program

---
+++ basic program/loops/simple_counting_no.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main ()
+{
+    int a, b;
+    printf("Enter the value: ");
+    scanf("%d", &a);
+
+    for (a = 0; a < b; a++)
+    // {
+    //     printf("*");
+    // }
+    
+    return 0;
+}
