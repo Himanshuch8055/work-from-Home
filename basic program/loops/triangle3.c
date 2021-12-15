ba77b9f50b48e3e964211a1ed1f640cb54f36fe4
Himanshu <codebreaker8055@gmail.com>
2021-12-15 22:49:45 +0330

    C Program

---
+++ basic program/loops/triangle3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter the row: ");
+    scanf("%d", &a);
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= a; j++)
+        {
+            if ((i + j) <= a)
+            {
+                printf(" ");
+            }
+            else
+                printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
