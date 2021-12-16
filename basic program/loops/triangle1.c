da30ad92f06aa9982f35aeddcce4bf4205f60f2c
Himanshu <codebreaker8055@gmail.com>
2021-12-16 18:18:53 +0330

    HOME

--- basic program/loops/triangle1.c
+++ basic program/loops/triangle1.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-     int b;
-     printf("Enter the number:");
-     scanf("%d", &b);
+    int b;
+    printf("Enter the number:");
+    scanf("%d", &b);
 
-     for (int i = 1; i <= b; i++)
-     {
-         for (int j = 1; j <= i; j++)
-         {
-             printf("*");
-         }
+    for (int i = 1; i <= b; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
 
-         printf("\n");
-     }
+        printf("\n");
+    }
 
     // Number Triangle
 
@@ -29,6 +29,38 @@ int main()
             printf("%d", j);
         }
 
+        printf("\n");
+    }
+
+    // Number triangle
+
+    int c;
+    printf("Enter the number:");
+    scanf("%d", &c);
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", i);
+        }
+
+        printf("\n");
+    }
+
+    // Number
+
+    int e, num=1;
+    printf("Enter the number:");
+    scanf("%d", &e);
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", num);
+            num++;
+        }
         printf("\n");
     }
 
